6c586c014108589ef5953810abb6b24d937825ef
Bob <bob@gmail.com>
2024-06-27 20:06:08 +0300

    Добавлены подтвиты

--- script.js
+++ script.js
@@ -1,27 +1,68 @@
+class Tweet {
+    constructor(tweet) {
+        this.id = tweet.id;
+        this.authorTag = tweet.authorNickname;
+        this.authorName = tweet.authorName;
+        this.content = tweet.content;
+        // this.date = new Date();
+        this._tweetElement = createTweetElement(tweet); //после создания добавить див и класс для сабтвитов(реплайев)
+        this._replyElementList = [];
+    }
+  
+    GetTweetElement(){
+        return this._tweetElement;
+    }
+
+    AddReply(tweetReply){
+        this._replyElementList.push(tweetReply);
+        this._tweetElement.appendChild(tweetReply.GetTweetElement());
+    }
+    
+  }
+
 // Пример данных твитов
 const tweets = [
     {
-        authorName: "Shreck_Ogr",
-        authorNickname: "@shrecksy",
+        parent: "",
+        id: "@shreksi_1",
+        authorName: "Shrek_Ogr",
+        authorNickname: "@shreksi",
         authorImage: "Мощная_ава.jpg",
-        content: "Текст твита 1. Здесь содержится основное содержание твита."
+        content: "Кто сказал, что огры не умеют твитить? Размышляю о жизни в болоте и понимаю, что главное – быть собой. #ОгрПравда #ЖизньВБолоте"
     },
     {
-        authorName: "Shreck_Ogr",
-        authorNickname: "@1shrecksy",
+        parent: "",
+        id: "@shreksi_2",
+        authorName: "Shrek_Ogr",
+        authorNickname: "@shreksi",
         authorImage: "Мощная_ава.jpg",
-        content: "Текст твита 2. Здесь содержится основное содержание твита."
+        content: "Осел опять пытается петь караоке... Уши уже вянут. Пора искать новое место для отдыха. #ОгрЖизнь #ОселНеПой"
     },
 
-
+    {
+        parent: "@shreksi",
+        id: "@shreksi_@Oslik<3_1",
+        authorName: "Oslik<3",
+        authorNickname: "@papaAngelochkov",
+        authorImage: "Icon_Osel.jpg",
+        content: " Эй, @shreksi! Просто завидуешь моим вокальным талантам! Могу дать пару уроков, если захочешь."
+    },
     
 ];
 
+//шрек = 0 осел = 1 
 var currentPageIndexGlobal = 0;
+
+function AddReply(){
+
+}
 
 function GetAuthorNicknameBy(currentPageIndex){
     if (currentPageIndex == 0){
-        return '@shrecksy';
+        return '@shreksi';
+    }
+    else if (currentPageIndex == 1){
+        return '@papaAngelochkov';
     }
     return 'undefined';
 }
@@ -62,17 +103,27 @@ function createTweetElement(tweet) {
     return tweetElement;
 }
 
-function renderTweets(tweets) {
+var tweetObjectList = [];
+
+function renderTweets(tweets) {    
+
     const tweetList = document.getElementById('tweetList');
-    var authorId = GetAuthorNicknameBy(currentPageIndexGlobal);
+    var currentPageauthorId = GetAuthorNicknameBy(currentPageIndexGlobal);
     tweets.forEach(tweet => {        
-        if (tweet.authorNickname == authorId)
+        if (tweet.authorNickname == currentPageauthorId && tweet.parent == "")
         {
-            const tweetElement = createTweetElement(tweet);
-            tweetList.appendChild(tweetElement);
+            var tweetObject = new Tweet(tweet);
+            tweetList.appendChild(tweetObject.GetTweetElement());
+            tweetObjectList.push(tweetObject);
         }
     });
 }
 
 // Вызов функции для рендеринга твитов при загрузке страницы
 renderTweets(tweets);
+
+function GetTweetById(tweets, tweetId){
+    return tweets.find(tweet => tweet.id === tweetId);
+}
+
+tweetObjectList[1].AddReply(new Tweet(GetTweetById(tweets, "@shreksi_@Oslik<3_1")));
